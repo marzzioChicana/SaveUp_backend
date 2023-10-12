fc9ad2b31aca7d6baeced44447f2637c003357d4
marzzioChicana <u202022228@upc.edu.pe>
2023-10-12 11:48:12 -0500

    fifth commit

--- src/main/java/com/upc/saveup/config/WebConfig.java
+++ src/main/java/com/upc/saveup/config/WebConfig.java
@@ -12,6 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("*"); // GET, POST, PUT, DELETE, PATCH
+                .allowedMethods("*")
+                .allowedHeaders(); // GET, POST, PUT, DELETE, PATCH
     }
 }
