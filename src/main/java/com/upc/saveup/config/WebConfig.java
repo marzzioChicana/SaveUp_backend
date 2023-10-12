52699595713e11ae76632870fb67a819b9468454
marzzioChicana <u202022228@upc.edu.pe>
2023-10-12 01:52:01 -0500

    second commit

--- src/main/java/com/upc/saveup/config/WebConfig.java
+++ src/main/java/com/upc/saveup/config/WebConfig.java
@@ -2,16 +2,16 @@ package com.upc.saveup.config;
 
         import org.springframework.context.annotation.Configuration;
         import org.springframework.web.servlet.config.annotation.CorsRegistry;
+        import org.springframework.web.servlet.config.annotation.EnableWebMvc;
         import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/*")
-                .allowedOrigins("https://saveupopen.netlify.app")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("")
-                .allowCredentials(true);
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*"); // GET, POST, PUT, DELETE, PATCH
     }
 }
